e621df76469db597b47358f585fd782e228d3f9c
“lupitasnoticias” <guav5695@colorado.edu>
2016-10-27 14:19:26 -0600

    added Js Scroll

---
+++ js/smooth.js
@@ -0,0 +1,23 @@
+'use strict';
+$(document).ready(function(){
+    //code goes here
+    console.log('hear me now jquery?');
+
+    //https://css-tricks.com/snippets/jquery/smooth-scrolling/
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+  
+  
+}); //end ready
